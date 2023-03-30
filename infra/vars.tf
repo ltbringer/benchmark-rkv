bd160e9f80d7eb67863b78ae45df00cc317c7bd8
ltbringer <amresh.venugopal@gmail.com>
2023-03-31 04:25:42 +0530

    add: variables for compute and network provisioning.

--- infra/vars.tf
+++ infra/vars.tf
@@ -3,5 +3,23 @@ variable "additional_volume_size" {
 }
 
 variable "volume_type" {
-  default = "ssd"
+  # Read about volume types here:
+  # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-volume-types.html
+  default = "gp3"
+}
+
+variable "instance_type" {
+  default = "c6a.2xlarge"
+}
+
+variable "ami" {
+  default = "ami-02eb7a4783e7e9317"
+}
+
+variable "delete_on_termination" {
+  default = true
+}
+
+variable "availability_zone" {
+  default = "ap-south-1a"
 }

f0bfabd80a9188754ae1f2410bcd154c134942ef
ltbringer <amresh.venugopal@gmail.com>
2023-03-31 04:26:57 +0530

    update: ec2 parameters.

--- infra/compute.tf
+++ infra/compute.tf
@@ -1,8 +1,10 @@
-resource "aws_instance" "example" {
-  ami             = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  key_name        = "example_key"
-  security_groups = ["example_security_group"]
+resource "aws_instance" "benchmark_instance" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = aws_key_pair.rkv_benchmark_key.id
+  security_groups = [aws_security_group.ssh_into_benchmark_instance.id]
+  vpc_security_group_ids = [aws_security_group.ssh_into_benchmark_instance.id]
+  subnet_id       = aws_default_subnet.default.id
   tags = {
     Name = "benchmark_rkv"
   }
@@ -11,6 +13,6 @@ resource "aws_instance" "example" {
     device_name           = "/dev/sdf"
     volume_size           = var.additional_volume_size
     volume_type           = var.volume_type
-    delete_on_termination = true
+    delete_on_termination = var.delete_on_termination
   }
 }

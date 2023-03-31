8adb44c18018e07a3b69d43330d1534833d66fe4
ltbringer <amresh.venugopal@gmail.com>
2023-04-01 00:17:57 +0530

    add: s3 buckets to contain reports.

---
+++ infra/bucket.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "benchmarks_bucket" {
+  bucket = "benchmarks-bucket"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "Benchmark Bucket"
+    Environment = "dev"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "benchmarks_bucket" {
+  bucket = aws_s3_bucket.benchmarks_bucket.id
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_s3_bucket_object" "benchmarks_bucket" {
+  bucket = aws_s3_bucket.benchmarks_bucket.id
+  key    = "rkv/reports/"
+  acl    = "public-read"
+}

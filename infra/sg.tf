18b2cde1057efc7183cc1f6866e97ff874a802de
ltbringer <amresh.venugopal@gmail.com>
2023-04-04 23:53:46 +0530

    update: allow egress anywhere

--- infra/sg.tf
+++ infra/sg.tf
@@ -8,4 +8,11 @@ resource "aws_security_group" "ssh_into_benchmark_instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
